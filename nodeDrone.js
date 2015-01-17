503d1b50aec5e5765285937bd76317dbcadc9f30
Jason Leung <jasleung@umich.edu>
2015-01-17 08:30:09 -0500

    last iteration with previous stdin interface... moving to keypress interface

--- nodeDrone.js
+++ nodeDrone.js
@@ -4,6 +4,7 @@ var prompt = require('prompt');
 var http = require('http');
 var fs = require('fs');
 var cv = require('opencv');
+var keypress = require('keypress');
 
 prompt.start();
 
@@ -26,9 +27,41 @@ process.stdin.on('data', function (chunk) {
         print("Taking off!");
         client.takeoff();
     }
-    else if(chunk === "l") {
+    else if(chunk === "u") {
         print("Landing...");
         client.land();
+    }
+    else if(chunk === "w") {
+        print("Ascending...");
+        client.up(.3);
+    }
+    else if(chunk === "s") {
+        print("Descending...");
+        client.down(.3);
+    }
+    else if(chunk === "a") {
+        print("Rotating Left...");
+        client.counterClockwise(.3);
+    }
+    else if(chunk === "d") {
+        print("Rotating Right...");
+        client.clockwise(.3);
+    }
+    else if(chunk === "i") {
+        print("Move Forward...");
+        client.front(.3);
+    }
+    else if(chunk === "k") {
+        print("Move Backward...");
+        client.back(.3);
+    }
+    else if(chunk === "j") {
+        print("Move Left...");
+        client.left(.3);
+    }
+    else if(chunk === "l") {
+        print("Move Right...");
+        client.right(.3);
     }
     else if(chunk == 'h') {
         print("Hovering");
@@ -42,7 +75,7 @@ process.stdin.on('data', function (chunk) {
     process.stdout.write("Enter command for AR Drone (t, l, h, or q): ");
 });
 
-var pngStream = client.getPngStream();
+/*var pngStream = client.getPngStream();
 var lastPng;
 
 var imgCounter = 0;
@@ -62,5 +95,5 @@ pngStream
                 console.log("The file was saved!");
             }
         }); 
-});
+});*/
 
