6c4c7969d0539433b257bd7dafc789afc340cc82
Maximilian Hemmrich <maximilian.hemmrich@gmail.com>
2015-01-18 11:54:50 -0500

    FACE TRACKING BRUH

--- nodeDrone.js
+++ nodeDrone.js
@@ -8,6 +8,9 @@ var path = require('path');
 
 var directory = "/Users/Max/Desktop/GitHub/MHacksV"; //get this programmatically later
 var result = "";
+var lastPng;
+var prevCenterX = -1, prevCenterY = -1;
+var faceCentered = false;
 
 
 function print(string) {
@@ -45,7 +48,86 @@ function executeFacialRecognition(filename, facex, facewidth, facey, faceheight)
 var client  = arDrone.createClient();
 client.disableEmergency();
 
+//FLAGS
 var flying = false;
+var setHeight = false;
+var viewLog = false;
+var startRotate = false;
+var stopRotate = false;
+var findPerson = false;
+var targetPerson = false;
+
+var desiredHeight = 1.6;
+
+function takingOff() {
+    print("taking off!");
+    client.takeoff(function() {
+        setHeight = true;
+        flying = true;
+    });
+}
+
+function foundFace() {
+    return true;
+}
+
+function getFaceLocation() {
+    if(lastPng && prevCenterX != -1 && prevCenterY != -1) {
+        faceCentered = false;
+
+        var imgWidth = 640;
+        var imgHeight = 360;
+
+        int colSection = -1;
+        int rowSection = -1;
+
+        //left column
+        if(prevCenterX >= 0 && prevCenterX < 210) {
+            colSection = 0;
+        }
+        //middle col
+        else if(prevCenterX >= 210 && prevCenterX < 430) {
+            colSection = 1;
+        }
+        //right col
+        else if(prevCenterX >= 430 && prevCenterX < imgWidth) {
+            colSection = 2;
+        }
+
+        //top row
+        if(prevCenterY >= 0 && prevCenterY < 120) {
+            rowSection = 0;
+        }
+        //middle row
+        else if(prevCenterY >= 120 && prevCenterY < 240) {
+            rowSection = 1;
+        }
+        //bottom row
+        else if(prevCenterY >= 240 && prevCenterY < 360) {
+            rowSection = 2;
+        }
+
+
+        if(rowSection == 0 && colSection == 0) return 1;
+        if(rowSection == 0 && colSection == 1) return 2;
+        if(rowSection == 0 && colSection == 2) return 3;
+        if(rowSection == 1 && colSection == 0) return 8;
+        if(rowSection == 1 && colSection == 1)  {
+            faceCentered = true;
+            return 9;
+        }
+        if(rowSection == 1 && colSection == 2) return 4;
+        if(rowSection == 2 && colSection == 0) return 7;
+        if(rowSection == 2 && colSection == 1) return 6;
+        if(rowSection == 2 && colSection == 2) return 5;
+    }
+    faceCentered = true;
+    return 9;
+}
+
+function faceInCenter() {
+    return faceCentered;
+}
 
 //Allow user to control drone's flight
 process.stdout.write("Enter command for AR Drone (t, l, h, or q): ");
@@ -55,7 +137,8 @@ process.stdin.on('data', function (chunk) {
 
     if(chunk === "t") {
         print("Taking off!");
-        client.takeoff(function() { flying = true; });
+        //client.takeoff(function() { flying = true; });
+        takingOff();
     }
     else if(chunk === "l") {
         print("Landing...");
@@ -103,13 +186,142 @@ process.stdin.on('data', function (chunk) {
         print("Down");
         client.down(0.3);
     }
+    else if(chunk == "log") {
+        print("Toggling log");
+        if(viewLog) viewLog = false;
+        else viewLog = true;
+    }
+    else if(chunk == "height") {
+      print("Adjusting height");
+      if(setHeight) setHeight = false;
+      else setHeight = true;
+    }
+    else if(chunk == "scan") {
+      print("Scanning for Fucking Criminals...");
+      startRotate = true;
+    }
+    else if(chunk == "find") {
+      print("Finding Fucking Criminals...");
+      findPerson = true;
+      takeOff();
+    }
     process.stdout.write("Enter command for AR Drone (t, l, h, or q): ");
+});
+
+
+client.on('navdata', function(data){
+  if(viewLog) {
+    if(data.demo){
+      console.log(data.demo.rotation);
+      console.log('frontBackDegrees: ' + data.demo.frontBackDegrees);
+      console.log('leftRightDegrees: ' + data.demo.leftRightDegrees);
+      console.log('clockwiseDegrees: ' + data.demo.clockwiseDegrees);
+    }
+    //console.log(data);
+  }
+
+  //Hovering at desired height
+  if(setHeight){
+    if(data.demo){
+      var currentHeight = data.demo.altitude;
+      if(currentHeight < desiredHeight - 0.05){
+        print("Going Up: " + currentHeight);
+        client.up(.2);
+      }else if(currentHeight > desiredHeight + 0.05){
+        print("Going Down: " + currentHeight);
+        client.down(.2);
+      }else{
+        print("Reached Desired Height");
+        setHeight = false;
+        client.stop();
+        if(findPerson) startRotate = true;
+      }
+    }
+  }
+
+  if(startRotate){
+    client.clockwise(0.2);
+    if(foundFace() && findPerson){
+      client.stop();
+      startRotate = false;
+      targetPerson = true;
+    }
+    if(stopRotate){
+      client.stop();
+      startRotate = false;
+      stopRotate = false;
+    }
+  }
+
+  if(targetPerson){
+    if(faceInCenter()){
+      client.stop();
+    }else{
+      switch(getFaceLocation()){
+        case 1:
+          client.stop();
+          print("1");
+          client.counterClockwise(.1);
+          client.up(.2);
+          sleep.sleep(500);
+          break;
+        case 2:
+          client.stop();
+          print("2");
+          client.up(.2);
+          sleep.sleep(500);
+          break;
+        case 3:
+          client.stop();
+          print("3");
+          client.clockwise(.1);
+          client.up(.2);
+          sleep.sleep(500);
+          break;
+        case 4:
+          client.stop();
+          print("4");
+          client.clockwise(.1);
+          sleep.sleep(500);
+          break;
+        case 5:
+          client.stop();
+          print("5");
+          client.down(.2);
+          client.clockwise(.1);
+          sleep.sleep(500);
+          break;
+        case 6:
+          client.stop();
+          print("6");
+          client.down(.2);
+          sleep.sleep(500);
+          break;
+        case 7:
+          client.stop();
+          print("7");
+          client.counterClockwise(.1);
+          client.down(.2);
+          sleep.sleep(500);
+          break;
+        case 8:
+          client.stop();
+          print("8");
+          client.counterClockwise(.1);
+          sleep.sleep(500);
+          break;
+        case 9:
+          print("9");
+          client.stop();
+          break;
+      }
+    }
+  }
 });
 
 
 //Save images from AR Drone's front facing camera
 var pngStream = client.getPngStream();
-var lastPng;
 var imgCounter = 0;
 var tmpCounter = 0;
 var location = "/Users/Max/Desktop/GitHub/MHacksV/tmp/";
@@ -123,7 +335,6 @@ pngStream
 
 //Facial recognition
 var processingImage = false;
-var prevCenterX = -1, prevCenterY = -1;
 var counter = 0;
 var detectFaces = function() {
     if(!processingImage && lastPng) {
